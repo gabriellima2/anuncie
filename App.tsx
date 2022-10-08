e1d13eeacbfe332433db9cf45517cbdd0601d7c6
Gabriel Lima <gabriel.rslima10@gmail.com>
2022-10-08 18:10:48 -0300

    configurações iniciais

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+
+export default function App() {
+	return (
+		<View>
+			<StatusBar style="auto" />
+
+			<Text>Open up App.js to start working on your app!</Text>
+		</View>
+	);
+}
