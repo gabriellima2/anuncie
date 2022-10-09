c50ccd44a12cf871124c311618a9c27267a144ce
Gabriel Lima <gabriel.rslima10@gmail.com>
2022-10-09 17:43:04 -0300

    feat: adiciona botão para trocar tema

---
+++ src/components/Buttons/ChangeThemeButton.tsx
@@ -0,0 +1,16 @@
+import { TouchableOpacity } from "react-native";
+import { useTheme } from "styled-components";
+
+import { Icon } from "../Icon";
+
+export const ChangeThemeButton = () => {
+	const { currentThemeName, changeTheme, colors } = useTheme();
+
+	const icon = currentThemeName === "dark" ? "moon-outline" : "sunny-outline";
+
+	return (
+		<TouchableOpacity onPress={changeTheme}>
+			<Icon name={icon} size={24} color={colors.main} />
+		</TouchableOpacity>
+	);
+};
