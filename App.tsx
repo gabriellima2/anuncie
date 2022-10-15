d85bc8c131c6af1d12f2cb6580e58e06e5ae321a
Gabriel Lima <gabriel.rslima10@gmail.com>
2022-10-15 16:31:29 -0300

    adiciona fontes

--- App.tsx
+++ App.tsx
@@ -1,7 +1,18 @@
+import { useFonts } from "expo-font";
+import { Text } from "react-native";
+
 import { ThemeContextProvider } from "./src/contexts/ThemeContext";
 import { Routes } from "./src/routes";
 
 export default function App() {
+	const [fontsLoaded] = useFonts({
+		SunflowerLight: require("./assets/fonts/Sunflower-Light.ttf"),
+		SunflowerMedium: require("./assets/fonts/Sunflower-Medium.ttf"),
+		SunflowerBold: require("./assets/fonts/Sunflower-Bold.ttf"),
+	});
+
+	if (!fontsLoaded) return <Text>Carregando...</Text>;
+
 	return (
 		<ThemeContextProvider>
 			<Routes />
