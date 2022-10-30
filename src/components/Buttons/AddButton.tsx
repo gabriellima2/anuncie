66bea2b2e20aeba4597f1309c7c09ac34c0d4fd1
Gabriel Lima <gabriel.rslima10@gmail.com>
2022-10-29 22:57:30 -0300

    troca icone do botão de adicionar ao carrinho

--- src/components/Buttons/AddButton.tsx
+++ src/components/Buttons/AddButton.tsx
@@ -5,10 +5,8 @@ import type { ButtonDefaultProps } from "../../types";
 
 interface AddButtonProps extends ButtonDefaultProps {}
 
-export const AddButton = (props: AddButtonProps) => {
-	return (
-		<Button {...props} size="small">
-			<Icon name="add" size={18} color={"#f1f1f1"} />
-		</Button>
-	);
-};
+export const AddButton = (props: AddButtonProps) => (
+	<Button {...props} size="small">
+		<Icon name="cart" size={18} color="#f1f1f1" />
+	</Button>
+);
