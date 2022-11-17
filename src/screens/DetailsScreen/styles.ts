f5a3e5f8c1c7394f879e6d53dc56d66ffee907ff
Gabriel Lima <gabriel.rslima10@gmail.com>
2022-11-17 16:52:37 -0300

    retira padding lateral dos textos de detalhes

--- src/screens/DetailsScreen/styles.ts
+++ src/screens/DetailsScreen/styles.ts
@@ -6,7 +6,7 @@ export const Container = styled.View`
 		flex: 1;
 		align-items: center;
 		justify-content: space-between;
-		padding: ${theme.spaces[3]};
+		padding: ${theme.spaces[3]} 0px;
 	`}
 `;
 
